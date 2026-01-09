72080d1267664787f1822ba10b5931f20e2d961f
WoojungBae <matt.woojung@gmail.com>
2026-01-09 01:36:32 -0500

    scaling for linAppox

--- tests/testthat/test-afttest.R
+++ tests/testthat/test-afttest.R
@@ -16,22 +16,22 @@ test_that("afttest linApprox=TRUE runs correctly", {
                    npath = 100, testType = "covForm", estMethod = "rr",
                    eqType = "ns", covTested = "z2", npathsave = 50,
                    linApprox = TRUE)
-  expect_equal(result$p_value, 0.05, tolerance=1e-1)
-  expect_equal(result$p_std_value, 0.04, tolerance=1e-1)
+  expect_equal(result$p_value, 0.00, tolerance=1e-1)
+  expect_equal(result$p_std_value, 0.00, tolerance=1e-1)
 
   result = afttest(object = Surv(Time, status) ~ z1 + z2, data = simdata,
                    npath = 100, testType = "covForm", estMethod = "rr",
                    eqType = "is", covTested = "z2", npathsave = 50,
                    linApprox = TRUE)
-  expect_equal(result$p_value, 0.07, tolerance=1e-1)
+  expect_equal(result$p_value, 0.00, tolerance=1e-1)
   expect_equal(result$p_std_value, 0.00, tolerance=1e-1)
 
   result = afttest(object = Surv(Time, status) ~ z1 + z2, data = simdata,
                    npath = 100, testType = "covForm", estMethod = "ls",
                    eqType = "ls", covTested = "z2", npathsave = 50,
                    linApprox = TRUE)
-  expect_equal(result$p_value, 0.13, tolerance=1e-1)
-  expect_equal(result$p_std_value, 0.12, tolerance=1e-1)
+  expect_equal(result$p_value, 0.01, tolerance=1e-1)
+  expect_equal(result$p_std_value, 0.00, tolerance=1e-1)
 })
 
 
@@ -70,6 +70,6 @@ test_that("afttest linApprox=FALSE runs correctly", {
                    npath = 100, testType = "covForm", estMethod = "ls",
                    eqType = "ls", covTested = "z2", npathsave = 50,
                    linApprox = FALSE)
-  expect_equal(result$p_value, 0.09, tolerance=1e-1)
-  expect_equal(result$p_std_value, 0.08, tolerance=1e-1)
+  expect_equal(result$p_value, 0.01, tolerance=1e-1)
+  expect_equal(result$p_std_value, 0.00, tolerance=1e-1)
 })
